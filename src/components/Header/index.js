38eb67e517bed19caf641b9b09aabc018453c1a3
Gustavo Moreira <gustavomoreira88@gmail.com>
2019-11-28 14:31:31 -0500

    Arquitetura Flux - Add items to cart.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,7 +8,7 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -17,10 +18,19 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 Itens</span>
+          <span>{cartSize} Itens</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
+
+/** Recebe o state (estado) do Header, retorna as informações que serão acessadas dentro do componente.
+ * Neste caso retorna para o componente o cart (carrinho de compras), do reducer (state.cart) que está na pasta store/modules/cart/reducer.js, será retornada a quantidade de elementos (length).
+ * Cada reducer cadastrado, pode ser acessado pelo state.
+ * Renderiza novamente todo componente quando o estado muda.
+ */
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,6 +24,15 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -35,7 +44,10 @@ class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
